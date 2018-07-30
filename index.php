feee8afa22fac85cd46bb2ae61d7b5c6c60a0a3f
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-07-30 15:39:02 +0200

    exo3p6 php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 3</title>
+  </head>
+  <body>
+    <p>
+<?php
+  echo $_GET['startDate'];
+?>
+  <br />
+<?php
+echo $_GET['endDate'];
+?>
+    </p>
+  </body>
+</html>
