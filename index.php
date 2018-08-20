84e86168b8ca3b88d7b5633ab2da387c48ea8cf9
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-08-20 10:32:25 +0200

    Correction avec la classe'

--- index.php
+++ index.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
-  <head>
-    <meta charset="utf-8" />
-    <title>Exercice 3</title>
-  </head>
-  <body>
-    <p>
-<?php
-/*Condition pour vérifier l'existence des paramètres*/
-  if(isset($_GET['startDate']) && isset($_GET['endDate'])){
-    /*Affichage des paramètres*/
-    echo $_GET['startDate'] . ' ' . $_GET['endDate'];
-  }
-?>
-    </p>
-  </body>
+    <head>
+        <meta charset="utf-8" />
+        <title>Exercice 3</title>
+    </head>
+    <body>
+        <p>
+            <?php
+//Condition pour vérifier l'existence des paramètres
+            if (!empty($_GET['startDate']) && !empty($_GET['endDate'])) {
+                //Affichage des paramètres
+                echo $_GET['startDate'] . ' - ' . $_GET['endDate'];
+            } else {
+                echo 'Il manque un paramètre';
+            }
+            ?>
+        </p>
+    </body>
 </html>
